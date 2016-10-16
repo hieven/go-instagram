ba15b5e11fdd5e1b05d4761033ac7f08dc503d92
Even <kiki44552002@gmail.com>
2016-10-16 22:19:42 +0800

    add `getRecentItems` of location

--- models/Location.go
+++ models/Location.go
@@ -13,21 +13,21 @@ type Location struct {
 	ExternalSource   string                `json:"external_source"`
 	City             string                `json:"city"`
 	Name             string                `json:"name"`
-	FacebookPlacesID int                   `json:"facebook_places_id"`
+	FacebookPlacesID int64                 `json:"facebook_places_id"`
 	Address          string                `json:"address"`
 	Lat              float64               `json:"lat"`
 	Lng              float64               `json:"lng"`
-	Pk               int                   `json:"pk"`
+	Pk               int64                 `json:"pk"`
 	Request          *gorequest.SuperAgent `json:"request"`
 }
 
 type mediaResponse struct {
 	RankedItems []*Media `json:"ranked_items"`
-	Items       []*Media `json:"media"`
+	Items       []*Media `json:"items"`
 }
 
 func (location Location) GetRankedMedia() []*Media {
-	url := constants.ROUTES.LocationFeed + strconv.Itoa(location.Pk) + "/"
+	url := constants.ROUTES.LocationFeed + strconv.FormatInt(location.Pk, 10) + "/"
 
 	_, body, _ := utils.WrapRequest(
 		location.Request.Get(url).
@@ -37,4 +37,17 @@ func (location Location) GetRankedMedia() []*Media {
 	json.Unmarshal([]byte(body), &resp)
 
 	return resp.RankedItems
+}
+
+func (location Location) GetRecentMedia() []*Media {
+	url := constants.ROUTES.LocationFeed + strconv.FormatInt(location.Pk, 10) + "/"
+
+	_, body, _ := utils.WrapRequest(
+		location.Request.Get(url).
+			Query("rank_token=" + utils.GenerateUUID()))
+
+	var resp mediaResponse
+	json.Unmarshal([]byte(body), &resp)
+
+	return resp.Items
 }
